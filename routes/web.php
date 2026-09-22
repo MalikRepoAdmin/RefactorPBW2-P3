ddb0b9b03a8cc2da3abf1f6002154fe6859ec546
raditCDlk <radityadp7224@gmail.com>
2026-09-22 20:29:33 +0700

    add: initiate new route directory and make new a controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TriaseController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function(){
+    return redirect()->route('traise.index');
 });
+
+Route::get('/traise', [TriaseController::class, 'index'])->name('traise.index');
+
+Route::post('/traise', [TriaseController::class, 'store'])->name('traise.store');
